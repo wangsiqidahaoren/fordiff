8467a60a51c22ac7acce6fa59385f418513a2882
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-16 19:33:21 +0800

    Add files via upload

--- wrpper.py
+++ wrpper.py
@@ -5,25 +5,13 @@ def make_buffer_allocation(self, buffer):
     size = self.codegen_shape_tuple(tuple(buffer.get_size()))
     stride = self.codegen_shape_tuple(tuple(buffer.get_stride()))
     if config.aot_inductor.abi_compatible:
-            device_type, device_id = device.split(",")
-            args = [
-                str(len(buffer.get_size())),
-                self.codegen_int_array_var(size, self.wrapper_call),
-                self.codegen_int_array_var(stride, self.wrapper_call),
-                dtype,
-                device_type,
-                "this->device_idx_" if V.graph.aot_mode else device_id,
-                f"&{name}_handle",
-            ]
-
-
-def make_buffer_allocation(self, buffer):
-        return self.make_allocation(
-            buffer.get_name(),
-            buffer.get_device(),
-            buffer.get_dtype(),
-            buffer.get_size(),
-            buffer.get_stride(),
-            self.can_cache_buffer_in_thread_local(buffer),
-        )
-
+        device_type, device_id = device.split(",")
+        args = [
+            str(len(buffer.get_size())),
+            self.codegen_int_array_var(size, self.wrapper_call),
+            self.codegen_int_array_var(stride, self.wrapper_call),
+            dtype,
+            device_type,
+            "this->device_idx_" if V.graph.aot_mode else device_id,
+            f"&{name}_handle",
+        ]
