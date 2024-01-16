2ef900fff4fcae02ec2135d47c3263e4622cd6d8
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-16 19:58:34 +0800

    Add files via upload

---
+++ mutual_info.py
@@ -0,0 +1,13 @@
+def _get_column(X, i):
+    if issparse(X):
+        x = np.zeros(X.shape[0])
+        start_ptr, end_ptr = X.indptr[i], X.indptr[i + 1]
+        x[X.indices[start_ptr:end_ptr]] = X.data[start_ptr:end_ptr]
+    else:
+        x = X[:, i]
+    return x
+def _iterate_columns(X, columns=None):
+    ...
+    for i in columns:
+        yield _get_column(X, i)
+    ...
