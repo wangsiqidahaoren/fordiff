bf6f6bbf3ae5d9025a926d5ba737df69c44a068d
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-17 20:15:52 +0800

    Add files via upload

--- APIExample.py
+++ APIExample.py
@@ -1,8 +1,5 @@
-def _get_variable_creator_initial_value(self,
-                                          replica_id,
-                                          device,
-                                          primary_var,
-                                          **kwargs):
-    if replica_id == 0:  # First replica on each worker.
-      assert device is not None
-      ...
+def _get_initial_value(self,
+    replica_id=0, device=None,
+    primary_var=None, **kwargs):
+    if replica_id == 0:
+        assert device is not None
