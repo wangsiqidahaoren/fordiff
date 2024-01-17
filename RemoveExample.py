f1222b5d5b9edf26687191d624d762d68580f509
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-17 15:59:30 +0800

    Remove dead code example

--- RemoveExample.py
+++ RemoveExample.py
@@ -3,9 +3,6 @@
 def guard_static_shapes(self, left: List[Expr]) -> List[int]:
     return [self.guard_static_shape(x) for x in left]
 
-def __getitem__(self, val: int) -> Expr:
-    return self.shape_env.duck_int(val)
-
 def size_hint(self, expr: Expr) -> int:
     if not isinstance(expr, Expr):
         assert isinstance(expr, int)
