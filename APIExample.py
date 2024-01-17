2a9b4fa2f371e8896500f808fa46c8e20b8aca11
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-17 14:41:35 +0800

    API Refactoring Example

--- APIExample.py
+++ APIExample.py
@@ -1,7 +1,7 @@
 def _get_variable_creator_initial_value(self,
-                                          replica_id=0,
-                                          device=None,
-                                          primary_var=None,
+                                          replica_id,
+                                          device,
+                                          primary_var,
                                           **kwargs):
     if replica_id == 0:  # First replica on each worker.
       assert device is not None
