df5eda04864dbd3ef835a8177c879d1d55c82bf4
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-17 20:16:43 +0800

    API Refactoring

--- APIExample.py
+++ APIExample.py
@@ -1,5 +1,5 @@
 def _get_initial_value(self,
-    replica_id=0, device=None,
-    primary_var=None, **kwargs):
+    replica_id, device,
+    primary_var, **kwargs):
     if replica_id == 0:
         assert device is not None
