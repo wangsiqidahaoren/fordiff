6e8a822fd0b29f5266b96934f81d3b3633dac3ae
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-17 11:00:58 +0800

    Add files via upload

---
+++ RenameMethodExample.py
@@ -0,0 +1,8 @@
+def rescale(self, image: np.ndarray, scale: Union[float, int]) -> np.ndarray:
+    self._ensure_format_supported(image)
+    return image * scal
+def to_numpy_array(self, image, rescale=None, channel_first=True):
+    ...
+    if rescale:
+        image = self.rescale(image.astype(np.float32), 1 / 255.0)
+    ...
