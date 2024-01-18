2f97920ca2281de263dcded3936c2b203df0ea3b
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-18 10:53:35 +0800

    Remove dead code example

--- RemoveExample.py
+++ RemoveExample.py
@@ -1,5 +1,3 @@
-def __getitem__(self, val: int) -> Expr:
-    return self.shape_env.duck_int(val)
 def size_hint(self, expr: Expr) -> int:
     if not isinstance(expr, Expr):
         assert isinstance(expr, int)
