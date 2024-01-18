171212e1b16db29c7af7d511de0283ffee644772
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-18 11:24:33 +0800

    Add files via upload

--- MoveMethodExample.py
+++ MoveMethodExample.py
@@ -1,16 +1,15 @@
---torch/distributed/fsdp/flat_param.py
+--torch/distributed/fsdp/fully_sharded_data_parallel.py
 ...
-def _get_shard(
-        tensor: Tensor,
-        rank: int,
-        world_size: int,
-    ) -> Tuple[Tensor, int]:
-
-    chunk, num_to_pad = FlatParamHandle._get_chunk(
+def _get_shard_functional(
+    tensor: torch.Tensor,
+    rank: int,
+    world_size: int,
+) -> Tuple[torch.Tensor, int]:
+    chunk, pad_num = FullyShardedDataParallel._get_chunk(
         tensor, rank, world_size,
     )
     shard = chunk.clone()
-    if num_to_pad > 0:
-        shard = F.pad(shard, [0, num_to_pad])
-    return shard, num_to_pad
+    if pad_num > 0:
+        shard = F.pad(shard, [0, pad_num])
+    return shard, pad_num
 ...
