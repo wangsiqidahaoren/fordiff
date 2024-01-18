9960b1fe928f9213a8d356e7720136c30c0483d4
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-18 10:48:50 +0800

    Add files via upload

--- RemoveExample.py
+++ RemoveExample.py
@@ -1,8 +1,5 @@
---torch/_inductor/sizevars.py
-...
-def guard_static_shapes(self, left: List[Expr]) -> List[int]:
-    return [self.guard_static_shape(x) for x in left]
-
+def __getitem__(self, val: int) -> Expr:
+    return self.shape_env.duck_int(val)
 def size_hint(self, expr: Expr) -> int:
     if not isinstance(expr, Expr):
         assert isinstance(expr, int)
