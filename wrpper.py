d5c81e29a9d6f3af821cb087a6f72205cfb77c0d
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-18 11:43:05 +0800

    Add files via upload

--- wrpper.py
+++ wrpper.py
@@ -1,28 +1,6 @@
 def make_buffer_allocation(self, buffer):
-    return self.make_allocation(
-        buffer.get_name(),
-        buffer.get_device(),
-        buffer.get_dtype(),
-        buffer.get_size(),
-        buffer.get_stride(),
-        self.can_cache_buffer_in_thread_local(buffer),
-    )
-
-def make_allocation(
-    self, name, device, dtype, shape, stride, can_cache_buffer_in_thread_local=False
-    ):
-    device = self.codegen_device(device)
-    dtype = self.codegen_dtype(dtype)
-    size = self.codegen_shape_tuple(shape)
-    stride = self.codegen_shape_tuple(stride)
-    if config.aot_inductor.abi_compatible:
-        device_type, device_id = device.split(",")
-        args = [
-            str(len(shape)),
-            self.codegen_int_array_var(size, self.wrapper_call),
-            self.codegen_int_array_var(stride, self.wrapper_call),
-            dtype,
-            device_type,
-            "this->device_idx_" if V.graph.aot_mode else device_id,
-            f"&{name}_handle",
-        ]
+    name = buffer.get_name()
+    device = buffer.get_device()
+    dtype = buffer.get_dtype()
+    size = buffer.get_size()
+    stride = tuple(buffer.get_stride())
