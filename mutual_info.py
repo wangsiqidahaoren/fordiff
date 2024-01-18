377c866ccae1a46e311c4c0357dcf00196dfaeb9
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-01-18 11:04:30 +0800

    Inline method

--- mutual_info.py
+++ mutual_info.py
@@ -1,13 +1,12 @@
 def _iterate_columns(X, columns=None):
     ...
-    for i in columns:
-        yield _get_column(X, i)
-    ...
-def _get_column(X, i):
     if issparse(X):
-        x = np.zeros(X.shape[0])
-        s_p, e_p = X.indptr[i], X.indptr[i + 1]
-        x[X.indices[s_p:e_p]] = X.data[s_p:e_p]
+        for i in columns:
+            x = np.zeros(X.shape[0])
+            s_p, e_p = X.indptr[i], X.indptr[i + 1]
+            x[X.indices[s_p:e_p]] = X.data[s_p:e_p]
+            yield x
     else:
-        x = X[:, i]
-    return x
+        for i in columns:
+            yield X[:, i]
+    ...
