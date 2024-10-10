99c62f222c9dfa10a041adad9c5f5503336c5f27
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-10-10 21:24:52 +0800

    Add files via upload

--- tests#test_tasks.py
+++ tests#test_tasks.py
@@ -28,11 +28,11 @@ def test_vector_clf():
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
 
-    model = Sequential()
-    model.add(Dense(nb_hidden, input_shape=(X_train.shape[-1],)))
-    model.add(Activation('relu'))
-    model.add(Dense(y_train.shape[-1]))
-    model.add(Activation('softmax'))
+    model = Sequential([
+        Dense(nb_hidden, input_shape=(X_train.shape[-1],), activation='relu'),
+        Dense(y_train.shape[-1], activation='softmax')
+    ])
+
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     history = model.fit(X_train, y_train, nb_epoch=15, batch_size=16,
                         validation_data=(X_test, y_test),
