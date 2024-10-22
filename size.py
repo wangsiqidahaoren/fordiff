f88f5248a80d859ea85a1cc964bf890be0052783
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2024-10-22 20:15:05 +0800

    Update size.py

--- size.py
+++ size.py
@@ -1,5 +1,5 @@
 class CropAndPad(meta.Augmenter):
-    ...    
+    ...
     def _augment_keypoints(self, keypoints_on_images, random_state, parents, hooks):
         result = []
         nb_images = len(keypoints_on_images)
@@ -7,14 +7,12 @@ class CropAndPad(meta.Augmenter):
         for keypoints_on_image, rng in zip(keypoints_on_images, rngs):
             height, width = keypoints_on_image.shape[0:2]
             samples = self._draw_samples_image(rng, height, width)
-
             kpsoi_aug = _crop_and_pad_kpsoi(
                 keypoints_on_image, croppings_img=samples.croppings,
                 paddings_img=samples.paddings, keep_size=self.keep_size)
             result.append(kpsoi_aug)
         return result
     ...
-
 def _crop_and_pad_kpsoi(kpsoi, croppings_img, paddings_img, keep_size):
     shifted = kpsoi.shift(
         x=-croppings_img[3]+paddings_img[3],
