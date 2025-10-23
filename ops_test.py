07af369111c320fe49e4d9b1f84a73a1eefc8159
Siqi Wang <66684937+wangsiqidahaoren@users.noreply.github.com>
2025-10-23 19:17:31 +0800

    Update ops_test.py

--- ops_test.py
+++ ops_test.py
@@ -1,18 +1,17 @@
-class TTTensorTest(tf.test.TestCase):
+class _TTTensorTest():
   def testCastFloat(self)
   def testCastIntFloat(self)
-class TTMatrixTest(tf.test.TestCase):
+class _TTMatrixTest():
   def testCastFloat(self)
   def testCastIntFloat(self)
-class TTMatrixTestBatch(tf.test.TestCase):
+class _TTMatrixTestBatch():
   def testCastFloat(self):
     tt_mat = initializers.random_matrix_batch(((2, 3), (3, 2)), tt_rank=2, batch_size=3)
     with self.test_session() as sess:
-      for dtype in [tf.float16, tf.float32, tf.float64]:
-        casted = ops.cast(tt_mat, dtype)
-        casted_val = sess.run(ops.full(casted))
-        self.assertEqual(dtype, casted.dtype)
-        self.assertTrue(dtype, casted_val.dtype)
+      casted = ops.cast(tt_mat, self.tf_dtype)
+      casted_val = sess.run(ops.full(casted))
+      self.assertEqual(self.tf_dtype, casted.dtype)
+      self.assertTrue(self.tf_dtype, casted_val.dtype)
   def testCastIntFloat(self):
     np.random.seed(1)
     K_1 = np.random.randint(0, high=100, size=(1, 2, 2, 2))
@@ -21,8 +20,7 @@ class TTMatrixTestBatch(tf.test.TestCase):
     tt_int = TensorTrain([K_1, K_2, K_3], tt_ranks=[1, 2, 2, 1])
     tt_int_batch = shapes.expand_batch_dim(tt_int)
     with self.test_session() as sess:
-      for dtype in [tf.float16, tf.float32, tf.float64]:
-        casted = ops.cast(tt_int_batch, dtype)
-        casted_val = sess.run(ops.full(casted))
-        self.assertEqual(dtype, casted.dtype)
-        self.assertTrue(dtype, casted_val.dtype)
+      casted = ops.cast(tt_int_batch, self.tf_dtype)
+      casted_val = sess.run(ops.full(casted))
+      self.assertEqual(self.tf_dtype, casted.dtype)
+      self.assertTrue(self.tf_dtype, casted_val.dtype)
